0f1b34418e31df2e4d178798eea732b2a2c58d6f
김명근 <rlaaudrms369@naver.com>
2026-02-09 16:37:52 +0900

    2월 9일 퀴즈

--- 2월/02.09/untitled/src/main/java/Main.java
+++ 2월/02.09/untitled/src/main/java/Main.java
@@ -4,36 +4,38 @@ import java.io.*;
 class Adventurer {
     // 인스턴스 변수 : 객체마다 별도로 저장됨(전사, 마법사 등), 힙 영역
     String name; // 모험가의 이름
-    int level = 1; // 모험가의 레벨
+    int level; // 모험가의 레벨
 
     // 클래스 변수(static) : 모든 객체가 공유함, 스택 영역
     static int totalCount = 0; // 현재 생성된 모험가의 수
 
     // 생성자
-    public Adventurer(String adventurerName) {
+    Adventurer(String name) {
+        this.name = name;
+        this.level = 1;
         totalCount++; // 생성자가 호출될 때마다 값을 누적
-        System.out.println("모험가 " + adventurerName + "이(가) 생성되었습니다");
+        System.out.println("모험가 " + this.name + "이(가) 생성되었습니다.");
     }
 
     // 메서드 오버로딩 -> 이름은 같지만 매개변수가 다름 (공격) 2가지 패턴
-    public void attack() {
-        System.out.println("맨손으로 공격합니다!");
+    void attack() {
+        System.out.println(name + "이(가) 맨손으로 공격합니다!");
     }
 
-    public void attack(String weapon) {
-        System.out.println("무기로 공격합니다!");
+    void attack(String weapon) {
+        System.out.println(name + "이(가)" + weapon + "(으)로 공격합니다!");
     }
 
 
     // 가변인자 : 아이템 개수가 몇 개 들어올지 모를 때 사용 -> 아이템 획득
-    public void getLoot(String... items) {
+    void getLoot(String... items) {
         for(String item : items) {
             System.out.println("아이템 " + item + "을(를) 획득하였습니다!");
         }
     }
 
     // static 메서드 : 인스턴스 변수(name등은 사용 불가). static 변수만 사용 가능하게
-    public static void printTotalCount() {
+    static void printTotalCount() {
         System.out.println("현재 생성된 총 모험가의 수 : " + totalCount);    // 현재 서버의 총 모험가 수 출력
     }
 }
@@ -42,21 +44,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
+        // static 메서드 확인 (객체 생성 전 호출 가능)
+        Adventurer.printTotalCount();
+
         // 객체 생성 (생성자 호출 시 static 변수 증가)
-        Adventurer Warrior = new Adventurer("마법사");
-        Adventurer Magician = new Adventurer("전사");
+        Adventurer Warrior = new Adventurer("전사");
+        Adventurer Magician = new Adventurer("마법사");
 
         // 메서드 오버로딩 테스트
-        String weapon = "";
-        Warrior.attack();
-        Warrior.attack(weapon);
+        Magician.attack();
+        Warrior.attack("검");
 
         // 가변인자 테스트
-        String item1 = "검";
-        String item2 = "지팡이";
-        String item3 = "표창";
-        Magician.getLoot(item1, item2, item3);
+        Magician.getLoot("검", "지팡이", "표창");
 
         // static 변수 공유 확인
         Adventurer.printTotalCount();
